a0fedca60d5807b2cf0b13dad5124bdf80ba8a24
Muzaffar Sharofitdinov <smartboymuzaffar@gmail.com>
2025-02-18 21:43:25 +0900

    Updated

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tiny_task",
-    version="1.0",
-    author="Your Name",
+    name="pytinytask",
+    version="1.7",
+    author="Smart Boy",
     description="A simple macro recording and replaying tool",
+    long_description=open('README.md').read(),  # Or .rst if you're using reStructuredText
+    long_description_content_type='text/markdown',  # Set to 'text/rst' if you're using reStructuredText
     packages=find_packages(),
     install_requires=[
         "keyboard",
@@ -13,14 +15,25 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'tiny_task = tiny_task:main'
-            #'tiny_task = tiny_task:main'
+            'tiny_task = tinytask:main',
+            'tinytask = tinytask:main',
+            'tiny-task = tinytask:main',
         ]
     },
+    python_requires=">=3.6",
     classifiers=[
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
     ],
-    python_requires='>=3.6',
 )
